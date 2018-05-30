fe0d33ddc8577c839e4a8d609629bb1f4e9b89d5
Ivaylo Gospodinov <ivaylo.igconsultancyservices@gmail.com>
2018-05-30 13:44:54 +0100

    HttpClient, HttpResponse, HttpErrorResponse, Observables and rxjs operators imported so available to use. Interface created. HttpClient injected into the constructor so available to use below. GetConfig methods implemented --> two variants (with or without observables) + GetConfigResponse method. As well error handler method implemented to catch if the backend return unsuccessful response or if on the client side error occurred.

---
+++ src/app/config/config.service.ts
@@ -0,0 +1,61 @@
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {HttpErrorResponse, HttpResponse} from '@angular/common/http';
+
+import {catchError, retry} from 'rxjs/internal/operators';
+import {throwError, Observable} from 'rxjs';
+
+export interface Config {
+  heroesUrl: string;
+  textfile: string;
+}
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ConfigService {
+
+  constructor(private http: HttpClient) {
+  }
+
+  configUrl = 'assets/config.json';
+
+  getConfig() {
+    return this.http.get(this.configUrl);
+  }
+
+  // Observables returned by the HttpClient methods and pipe them through to the error handler.
+  getConfig_v1() {
+    return this.http.get<Config>(this.configUrl)
+      .pipe(
+        retry(3), // automatically re-subscribes to a failed Observable a specified number of times
+        catchError(this.handleError)
+      );
+  }
+
+  // Tell HttpClient that you want the full response with the observe option
+  // And HttpClient.get() returns an Observable of typed HttpResponse rather than just the JSON data.
+  getConfigResponse(): Observable<HttpResponse<Config>> {
+    return this.http.get<Config>(this.configUrl, {observe: 'response'});
+  }
+
+  // Error inspection, interpretation, and resolution is something you want to do in the service, not in the component.
+  private handleError(error: HttpErrorResponse) {
+    if (error.error instanceof ErrorEvent) {
+      // A client-side or network error occurred. Handle it accordingly.
+      console.error('An error occurred: ', error.error.message);
+    } else {
+      // The backend returned an unsuccessful response code.
+      // The response body may contain clues as to what went wrong,
+      console.error(
+        `Backend returned code ${error.status}, ` +
+        `body was ${error.error}`
+      );
+    }
+    // return an observable with a user-facing error message
+    return throwError('Something bad happened, please try again later.');
+  }
+
+
+}
