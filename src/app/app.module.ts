defd32ae036c7df8255056fef67415a6f35106f8
Ivaylo Gospodinov <ivaylo.igconsultancyservices@gmail.com>
2018-05-31 08:01:29 +0100

    updating the root module..

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,33 @@
+// modules
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 import {HttpClientModule} from '@angular/common/http';
 
+// components
 import {AppComponent} from './app.component';
-import { ConfigComponent } from './config/config.component';
+import {ConfigComponent} from './config/config.component';
+import {DownloaderComponent} from './downloader/downloader.component';
+import { HeroesComponent } from './heroes/heroes.component';
+
+// services
+import {ConfigService} from './config/config.service';
+import {DownloaderService} from './downloader/downloader.service';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ConfigComponent
+    ConfigComponent,
+    DownloaderComponent,
+    HeroesComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule
   ],
-  providers: [],
+  providers: [
+    ConfigService,
+    DownloaderService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule {
