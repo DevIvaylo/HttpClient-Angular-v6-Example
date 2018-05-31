69791173313baa03a9d6ca7e1b30812f615e9f22
Ivaylo Gospodinov <ivaylo.igconsultancyservices@gmail.com>
2018-05-31 15:21:10 +0100

    updating the root module..

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,31 +2,40 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 import {HttpClientModule} from '@angular/common/http';
+import {FormsModule} from '@angular/forms';
 
 // components
 import {AppComponent} from './app.component';
 import {ConfigComponent} from './config/config.component';
 import {DownloaderComponent} from './downloader/downloader.component';
-import { HeroesComponent } from './heroes/heroes.component';
+import {HeroesComponent} from './heroes/heroes.component';
 
 // services
 import {ConfigService} from './config/config.service';
 import {DownloaderService} from './downloader/downloader.service';
+import {HeroesService} from './heroes/heroes.service';
+import {HttpErrorHandler} from './http-error-handler.service';
+import { PackageSearchComponent } from './package-search/package-search.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     ConfigComponent,
     DownloaderComponent,
-    HeroesComponent
+    HeroesComponent,
+    PackageSearchComponent
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
   providers: [
     ConfigService,
-    DownloaderService
+    DownloaderService,
+    HeroesService,
+    HttpErrorHandler
   ],
   bootstrap: [AppComponent]
 })
